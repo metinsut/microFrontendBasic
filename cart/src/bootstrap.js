3c1be9f0f2bbf612ba32b2a7972166d49b8b1d6a
Metin Süt <metindir@gmail.com>
2021-05-13 21:29:46 +0300

    first commit

---
+++ cart/src/bootstrap.js
@@ -0,0 +1,16 @@
+import faker from 'faker';
+
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.datatype.number()} items in your cart</div>`;
+
+  el.innerHTML = cartText;
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const el = document.getElementById('dev-cart');
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };
